75a152434ee8c16e63212700a67cf70d41bf7286
2020ashish <arajanand29@gmail.com>
2025-07-01 16:05:44 +0530

    CMX_SAST: Add Support for Regex in Project Name

--- cx-onprem-sast-job/checkmarx_job.py
+++ cx-onprem-sast-job/checkmarx_job.py
@@ -134,24 +134,73 @@ class Checkmarx:
                 log.error(f"<error> Unknow error {response.json()} </error> ")
                 sys.exit(1)
 
-    def _fetch_checkmarx_projects(self, project=None):
+    def build_rule_sets(self, patterns):
+        try:
+            if isinstance(patterns, str):
+                patterns = [patterns]
+
+            include = []
+            exclude = []
+
+            for pat in patterns:
+                if not pat:
+                    continue
+                pat = pat.strip()
+                is_exclude = pat.startswith('-')
+                if is_exclude:
+                    pat = pat[1:]
+
+                # Handle "*"
+                if pat == "*":
+                    regex = ".*"
+                elif pat.startswith("*") and pat.endswith("*"):
+                    regex = f".*{re.escape(pat.strip('*'))}.*"
+                elif pat.startswith("*"):
+                    regex = f".*{re.escape(pat.lstrip('*'))}$"
+                elif pat.endswith("*"):
+                    regex = f"^{re.escape(pat.rstrip('*'))}.*"
+                else:
+                    regex = f"^{re.escape(pat)}$"
+
+                if is_exclude:
+                    exclude.append(f"(?!{regex})")
+                else:
+                    include.append(regex)
+
+            if not include and exclude:
+                pattern = "^" + "".join(exclude) + ".*$"
+            elif include:
+                pattern = "^" + "".join(exclude) + "(" + "|".join(include) + ")$"
+            else:
+                pattern = "^.*$"
+
+            return re.compile(pattern)
+        except Exception as e:
+            raise ValueError(f"Invalid format CX_PROJECT: {e}")
+
+    def _fetch_checkmarx_projects(self):
         """
         Fetch projects from Checkmarx API.
         :return: JSON response containing project data
         """
-        log.info(f"<info> Fetching a projects details for {project} ... </info>")
-        limit = 1
-        offset = 0
-        endpoint = f"projects/?limit={limit}&offset={offset}&projectName={project}"
+        log.info(f"<info> Fetching a projects details ... </info>")
+        pattern = self.env["CX_PROJECT"]
+        endpoint = f"projects"
         data = self._fetch_data(endpoint)
+        message = []
+        project_data = []
         if isinstance(data, list) and len(data) > 0:
-            project_data = data[0]
-            return project_data, project_data.get("id")
-        elif isinstance(data, str):
-            return {}, None
-        else:
-            log.error(f"<Info> There is unknown issue: {data} </error>")
-            return {}, None
+            rule_set = self.build_rule_sets(pattern)
+            for project in data:
+                name = project.get("name")
+                if rule_set and rule_set.match(name):
+                    project_data.append(project)
+                    message.append(name)
+
+        print("-"*30,"Collecting Data from these Projects","-"*30)
+        for proj in message:
+            print(f"Project: {proj}")
+        return project_data
 
     def _fetch_last_scan_id(self, project_id):
         log.info(
@@ -337,22 +386,24 @@ class Checkmarx:
         if not self.bearer_token:
             log.error("Failed to retrieve Checkmarx bearer token.")
             return
-        log_info = []
-        for project_name in self.env["CX_PROJECT_NAMES"]:
+        message = []
+        projects = self._fetch_checkmarx_projects()
+        for project in projects:
             print("-"*90)
-            data, project_id = self._fetch_checkmarx_projects(project=project_name)
+            data = project
+            project_id = project.get("id")
+            project_name = project.get("name")
+            print("-"*20, f"project_name: {project_name}","-"*20)
             if not project_id:
                 msg = f"Invalid Project Name '{project_name}'"
                 log.warning(f"<warning> {msg} </warning>")
-                log_info.append(msg)
+                message.append(msg)
                 continue
-            scan_info, scan_ids  = self._fetch_last_scan_id(
-                project_id,
-            )
+            scan_info, scan_ids  = self._fetch_last_scan_id(project_id)
             if not scan_ids:
                 msg = f"No scans found for project '{project_name}'"
                 log.warning(f"<warning> {msg} </warning>")
-                log_info.append(msg)
+                message.append(msg)
                 continue
 
             data["scan"] = scan_info
@@ -362,16 +413,22 @@ class Checkmarx:
                 issues_file = os.path.join(SCANNED_FILE_DIR, f"CHECKMARX-CX-{time_suffix}.json")
                 with open(issues_file, "w") as f:
                     json.dump(data, f, indent=2)
-                log.info(f"Results written to {issues_file}")
-                self.upload_results(issues_file)
-                log_info.append(f"Results uploaded for project '{project_name}''")
+                upoad_status, msg = self.upload_results(issues_file)
+                if upoad_status:
+                    msg = f"Results uploaded for project '{project_name}'"
+                else:
+                    log.error(f"<error> Upload failed: {msg} </error>")
+                    sys.exit(1)
+                message.append(msg)
+            except Exception as e:
+                log.error(f"Error while saving or uploading results for {project_name}: {e}")
+            finally:
                 ## remove the json file
                 if os.path.exists(issues_file):
                     os.remove(issues_file)
-            except Exception as e:
-                log.error(f"Error while saving or uploading results for {project_name}: {e}")
+
         print("-"*30,"Result","-"*30)
-        print("\n".join(log_info))
+        print("\n".join(message))
 
     def upload_results(self, result_file):
         log.info(f"<info> Uploading the result to AccuKnox control plane... </info>")
@@ -384,7 +441,8 @@ class Checkmarx:
                 shutil.copyfileobj(f_in, f_out)
 
             with open(result_file, 'rb') as file:
-
+                data = file.read()
+                files = {"file": (result_file, data)}
                 url = f"{self.env["AK_ENDPOINT"]}/api/v1/artifact/"
                 headers={
                         "Tenant-Id": self.env["AK_TENANT_ID"],
@@ -396,19 +454,39 @@ class Checkmarx:
                         "save_to_s3": "false",
                         "label_id": self.env["AK_LABEL"],
                     }
-                files = {'file': (result_file, file)}
-                try:
-                    response = requests.post(url=url, files=files, headers=headers, params= params, verify=False)
-                except SSLError:
-                    response = requests.post(url=url, files=files, headers=headers, params= params)
+
+                last_exc = None
+                for attempt in range(1, 4):
+                    try:
+                        try:
+                            response = requests.post(url=url, files=files, headers=headers, params= params, verify=False)
+                        except SSLError:
+                            response = requests.post(url=url, files=files, headers=headers, params= params)
+                        response.raise_for_status()
+                        log.info(f"<info> Upload successful on attempt {attempt}. Status: {response.status_code} </info>")
+                        break
+                    except (requests.exceptions.RequestException, HTTPError) as e:
+                        last_exc = e
+                        time.sleep(1)
+                        log.warning(f"Upload attempt {attempt} failed: {e}")
+                else:
+                    # all attempts failed
+                    raise last_exc
             response.raise_for_status()
             log.info(f"<info> Upload successful. Response: {response.status_code} </info>")
+            return True, ""
         except HTTPError as http_err:
-           log.error(f"<error> Status code: {response.status_code}, Response: {response.text} </error>")
+           msg = f"HTTP error: received status code: {response.status_code}, Response body: {response.text}"
+           log.error(f"<error> {msg} </error> ")
+           return False, msg
         except requests.exceptions.RequestException as req_err:
-            log.error(f"<error>403 Request exception occurred: {req_err} </error>")
+            msg = f"Network error: failed to complete request. Details: {req_err}"
+            log.error(f"<error> {msg} </error> ")
+            return False, msg
         except Exception as err:
-            log.error(f"<error>Unexpected error occurred: {err} </error>")
+            msg = f"Unexpected error: {err}"
+            log.error(f"<error> {msg} </error> ")
+            return False, msg
         finally:
             # Remove temp gzip file
             if os.path.exists(compressed_path):
@@ -419,7 +497,7 @@ class Checkmarx:
 
 
 REQUIRED_ENV_VARS = [
-    "CX_PROJECT_NAMES",
+    "CX_PROJECT",
     "CX_BASE_URL",
     "CX_USER_NAME",
     "CX_PASSWORD",
@@ -444,16 +522,18 @@ def get_env_config():
 
     config = {key: os.environ.get(key) for key in REQUIRED_ENV_VARS}
 
-    # Parse CX_PROJECT_NAMES as list from comma-separated string
-    config["CX_PROJECT_NAMES"] = [
-        name.strip() for name in config["CX_PROJECT_NAMES"].split(",") if name.strip()
-    ]
-
+    # Parse CX_PROJECT as list from comma-separated string
+    raw_project_map =  os.environ.get("CX_PROJECT","").strip()
+    try:
+        project_name = json.loads(raw_project_map)
+    except Exception as e:
+        raise ValueError(f"Invalid format CX_PROJECT: {e}")
+    config["CX_PROJECT"] = project_name
     for key, default in OPTIONAL_ENV_DEFAULTS.items():
         config[key] = os.environ.get(key, default)
 
-    return config
 
+    return config
 
 if __name__ == "__main__":
     env = get_env_config()
